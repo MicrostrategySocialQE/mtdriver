fab3567a4e1870bffe9f4b99a4cf393cfd0cbea2
dfeng <dfeng@microstrategy.com>
2014-03-25 10:54:59 +0800

    Update setup.py
    new version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='MTDriver',
-    version='0.02',
+    version='0.03',
     author='Mingze',
     author_email='mzxu@outlook.com',
     packages=['MTDriver'],
